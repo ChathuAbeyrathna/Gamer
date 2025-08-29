3fb2ed1886322ad95916bfab24fdc01fddd823e6
ChathuAbeyrathna <abeyrathnamrcd.21@uom.lk>
2025-08-29 17:24:48 +0530

    card changes

--- gamer_frontend/src/components/FeedCard.jsx
+++ gamer_frontend/src/components/FeedCard.jsx
@@ -383,7 +383,6 @@ const FeedCard = ({
                   </button>
                   <button className="flex items-center w-full text-left px-4 py-2 hover:bg-gray-600">
                     <div className="bg-gray-100 rounded-full w-4 h-4 flex items-center justify-center text-black mr-2">!</div>
-                    <span>Report</span>
                   </button>
                 </>
               )}
